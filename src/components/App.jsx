77d651d1b4246228dc697e6954e61d2b13d9efeb
Florie Lesueur <florie.lesueur@gmail.com>
2020-01-19 22:05:27 +0100

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Presentation from './Presentation';
+
+const App = () => {
+
+    return(
+        <>
+            <Presentation/>
+        </>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import image from '../assets/images/stairs.jpg'
+
+import App from './components/App';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-image: linear-gradient(to bottom, rgba(255,255,255,0.5),rgba(255,255,255,0.3)), url(${image});
+        background-size: contain;
+        background-attachment: fixed;
+    }
+`;
+
+ReactDOM.render(
+    <>
+        <GlobalStyle/>
+        <App />
+    </>,
+    document.getElementById('root')
+);
